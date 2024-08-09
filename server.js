3b2da5b74a45d176228028061015e95f8841874e
nikhil kalsariya <nickahir0786@gmail.com>
2024-08-09 09:30:03 +0530

    09/08/24

--- server.js
+++ server.js
@@ -1,45 +1,22 @@
 const express = require('express');
 const server = express();
 
-server.get("/", (req, res) => {
-    res.write('welcome to express server');
+server.get('/', (req, res) => {
+        res.write("Welcome Express Js");
+        res.end();
+})
+
+server.get('/login', (req, res) => {
+    res.write("Welcome Express Js");
     res.end();
 })
 
-// server.get("/",(req,res)=>{
-//     res.send('get method-1');
-// })
-
-server.get("/user", (req, res) => {
-    res.status(200);
-    res.json({ message: 'use GET method' });
-})
-server.post("/user", (req, res) => {
-    res.status(201);
-    res.json({ message: 'use POST method' });
-})
-server.put("/user", (req, res) => {
-    // res.status(201);
-    res.json({ message: 'use PUT method' });
-})
-server.patch("/user", (req, res) => {
-    // res.status(201);
-    res.json({ message: 'use PATCH method' });
-})
-server.delete("/user", (req, res) => {
-    // res.status(201);
-    res.json({ message: 'use DELETE method' });
-})
-server.patch("/admin", (req, res) => {
-    // res.status(201);
-    res.json({ message: 'Admin patch method' });
-})
-server.delete("/admin", (req, res) => {
-    // res.status(201);
-    res.json({ message: 'Admin DELETE method' });
+server.get('/login', (req, res) => {
+    res.json({msg:"Login page post"});
+    res.end();
 })
 
-server.listen(1234, () => {
-    console.log('server start at http://localhost:1234');
-
+server.listen(3500,()=>{
+    console.log(`Server Start at http://localhost:3500`);
+    
 })
