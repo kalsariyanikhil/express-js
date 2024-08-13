d554db5f0ed2e60d6fe7343dcf946101e6993009
nikhil kalsariya <nickahir0786@gmail.com>
2024-08-13 09:31:23 +0530

    basic

--- server.js
+++ server.js
@@ -11,26 +11,39 @@ server.get("/", (req, res) => {
     res.send('welcome to express server');
 })
 
-// crud 
+//replace Data - Put
 
-// add new product - create 
-server.post("/product", (req, res) => {
-    // console.log(req.body);
-    products.push(req.body);
-    res.json({ product: req.body, Message: 'product added sucess' });
-});
-
-// get all products - read 
-server.get("/product", (req, res) => {
-    res.json(products);
-});
-
-// get single products - read 
-server.get("/product/:id", (req, res) => {
+server.put('/products/:id', (req, res) => {
     let id = +req.params.id;
-    let item = products.find((product) => product.id === id);
-    res.json(item);
+    let productIndex = products.findIndex((product) => product.id === id);
+    // console.log(productIndex);
+    products.splice(productIndex, 1, { ...product, ...req.body });
+    res.json({ message: "Product Replace Success" })
 });
+
+// Update Data - Patch
+
+server.patch("/product/:id", (req, res) => {
+    let id = +req.params.id;
+    let productIndex = products.findIndex((product) => product.id === id);
+    //  console.log(productIndex);
+    const product = products[productIndex];
+    // console.log(product);
+    products[productIndex] = { ...products[productIndex], ...req.body };
+    res.json({ message: "Product Updated Success" })
+});
+
+// Delete Data - Delete
+
+server.delete("/products/:id", (req, res) => {
+    let id = +req.params.id;
+    let productIndex = products.findIndex((product) => product.id === id);
+    const product = products[productIndex];
+    products.splice(productIndex, 1);
+    res.json({ product, message: "Product Delete Success" })
+});
+
+
 
 server.listen(1234, () => {
     console.log("server start");
